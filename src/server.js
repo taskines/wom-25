81d4ff38e047e1f97cb4d379057fd6a369b188dd
taskines <taskines@arcada.fi>
2025-09-17 15:18:38 +0300

    Lektion 5 17.9

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
 const express = require('express')
+const PORT = process.env.PORT || 8080
 require('dotenv').config()
 
 const app = express()
-const PORT = process.env.PORT || 8080
+
 
 console.log(`Node.js ${process.version}`)
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.json({ msg: "Lektionsexempel 0.3" })
+app.get('/', async (req, res) => {
+  res.json({msg: "Lektionsexempel!"})
 })
 
 const notesRouter = require('./routes/notes')
